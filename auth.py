c27dbaee497573ea821f671efa03e8fa7a850ddf
Ashutosh <aashutoshgandhi13@gmail.com>
2024-10-15 16:39:48 +0530

    exp

--- auth.py
+++ auth.py
@@ -9,8 +9,21 @@ from main_app import main_app
 
 # Initialize Firebase Admin SDK
 if not firebase_admin._apps:
-    cred = credentials.Certificate('firebase_config.json')
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),  # Handle newline correctly
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["firebase"]["universe_domain"]
+    })
     firebase_admin.initialize_app(cred)
+
 
 # Firestore to store user info
 db = firestore.client()
